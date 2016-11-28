cb1b337a6905a0381762e6ee714d5ac9d3d2696c
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2016-11-28 12:54:32 +0530

    Update ReadUserGroupsFromOTMMAndWriteToExcel.java

--- src/main/java/com/otmm/custom/migration/ReadUserGroupsFromOTMMAndWriteToExcel.java
+++ src/main/java/com/otmm/custom/migration/ReadUserGroupsFromOTMMAndWriteToExcel.java
@@ -2,6 +2,7 @@ package com.otmm.custom.migration;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,75 +13,85 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.artesia.common.exception.BaseTeamsException;
-import com.artesia.entity.TeamsNumberIdentifier;
+import com.artesia.common.utils.LogUtils;
 import com.artesia.security.SecuritySession;
 import com.artesia.security.UserGroup;
 import com.artesia.security.services.UserGroupServices;
 import com.artesia.security.session.services.AuthenticationServices;
 
-public class ReadUserGroupsFromOTMMAndWriteToExcel {
+public class ReadUserGroupsFromOTMMAndWriteToExcel
+{
 
-	static XSSFWorkbook workbook = null;
-	private static XSSFSheet userGroupsSheet;
+    private static XSSFWorkbook workbook = null;
+    private static XSSFSheet userGroupsSheet;
+    private static int tableRownum = 0;
 
-	public static void findUserGroupsInOTMMAndWriteToExcel(String userName, String password, String teamsHome) {
+    public static void findUserGroupsInOTMMAndWriteToExcel(String userName,
+            String password, String teamsHome)
+    {
 
-		// Set TEAMS_HOME value
-		if (System.getenv("TEAMS_HOME") != null) {
-			System.setProperty("TEAMS_HOME", System.getenv("TEAMS_HOME"));
-		} else {
-			System.setProperty("TEAMS_HOME", teamsHome);
-		}
+        // Set TEAMS_HOME value
+        if (System.getenv("TEAMS_HOME") != null) {
+            System.setProperty("TEAMS_HOME", System.getenv("TEAMS_HOME"));
+        }
+        else {
+            System.setProperty("TEAMS_HOME", teamsHome);
+        }
 
-		try {
+        try {
 
-			// Blank workbook
-			workbook = new XSSFWorkbook();
+            // Blank workbook
+            workbook = new XSSFWorkbook();
 
-			// Create a blank sheet
-			userGroupsSheet = workbook.createSheet("UserGroups");
+            // Create a blank sheet
+            userGroupsSheet = workbook.createSheet("UserGroups");
 
-			List<String> headerTitle = new ArrayList<String>();
-			headerTitle.addAll(Arrays.asList("UserGroup Name", "UserGroup Description"));
+            List<String> headerTitle = new ArrayList<String>();
+            headerTitle.addAll(Arrays.asList("UserGroup Name", "UserGroup Description"));
 
-			int tableRownum = 0;
-			XSSFRow tableRow = userGroupsSheet.createRow(tableRownum++);
+            XSSFRow tableRow = userGroupsSheet.createRow(tableRownum++);
 
-			int cellnum = 0;
-			for (String cellValue : headerTitle) {
-				XSSFCell cell = tableRow.createCell(cellnum++);
-				cell.setCellValue(cellValue);
-			}
+            int cellnum = 0;
+            for (String cellValue : headerTitle) {
+                XSSFCell cell = tableRow.createCell(cellnum++);
+                cell.setCellValue(cellValue);
+            }
 
-			readUserGroupsFromOTandWriteInExcel(userName, password);
+            readUserGroupsFromOTandWriteInExcel(userName, password);
 
-			FileOutputStream out = new FileOutputStream(new File("MigrationSheet.xlsx"));
-			workbook.write(out);
-			workbook.close();
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            FileOutputStream out = new FileOutputStream(new File("MigrationSheet.xlsx"));
+            workbook.write(out);
+            workbook.close();
+            out.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (BaseTeamsException e) {
+            LogUtils.logException(e);
+        }
 
-	}
+    }
 
-	private static void readUserGroupsFromOTandWriteInExcel(String userName, String password)
-			throws BaseTeamsException {
-		SecuritySession session = null;
-		try {
-			session = AuthenticationServices.getInstance().login(userName, password);
-			UserGroup[] listOfUserGroups = UserGroupServices.getInstance().retrieveAllUserGroups(session);
-			for (UserGroup userGroups : listOfUserGroups) {
-				int tableNum = 1;
-				XSSFRow tableRow = userGroupsSheet.createRow(tableNum++);
-				XSSFCell nameCell = tableRow.createCell(0);
-				nameCell.setCellValue(userGroups.getName());
-				XSSFCell descriptionCell = tableRow.createCell(1);
-				descriptionCell.setCellValue(userGroups.getDescription());
-			}
-		} finally {
-			AuthenticationServices.getInstance().logout(session);
-		}
-	}
+    private static void readUserGroupsFromOTandWriteInExcel(String userName,
+            String password) throws BaseTeamsException
+    {
+        SecuritySession session = null;
+        try {
+            session = AuthenticationServices.getInstance().login(userName, password);
+            UserGroup[] listOfUserGroups = UserGroupServices.getInstance()
+                    .retrieveAllUserGroups(session);
+            for (UserGroup userGroups : listOfUserGroups) {
+                XSSFRow tableRow = userGroupsSheet.createRow(tableRownum++);
+                XSSFCell nameCell = tableRow.createCell(0);
+                nameCell.setCellValue(userGroups.getName());
+                XSSFCell descriptionCell = tableRow.createCell(1);
+                descriptionCell.setCellValue(userGroups.getDescription());
+            }
+        }
+        finally {
+            AuthenticationServices.getInstance().logout(session);
+        }
+    }
 
 }
